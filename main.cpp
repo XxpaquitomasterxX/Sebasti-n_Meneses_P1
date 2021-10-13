e1187dfa7bae51229c3d0c699773342d5ddcb0ce
Xxpaquitomaster <89699500+XxpaquitomasterxX@users.noreply.github.com>
2021-10-13 15:43:02 +0000

    jlhñkjhh

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Sebastián";
-  std::cout << "          ";
-  std::cout << "Meneses Castillo  ";
-  std::cout << "10-2B";
+  std::cout << "Sebastián\n";//aquí se enseña mi nombre/
+  std::cout << "          \n";//aquí se deja un espacio por no tener segundo nombre/
+  std::cout << "Meneses Castillo  \n";// Se escriben apellidos/
+  std::cout << "10-2B\n";//se escribe la sección/
 }
